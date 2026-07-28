c6c9c1354763c0543902f8abd43ee2e874f9a035
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-07-28 13:06:45 +0100

    bump to v0.0.4

--- Formula/cli-utils.rb
+++ Formula/cli-utils.rb
@@ -1,8 +1,8 @@
 class CliUtils < Formula
   desc "Custom utility commands."
   homepage "https://github.com/benmoose/homebrew-cli-utils"
-  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "2c0996e392673e76e300c5625045d0e13c96a5610bc70d32ed8d4fd4a088c305"
+  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "4248d2f8b2226546d9a0196f8524e4c75c9fb43644a613dfabe0d72e7a8e8bcf"
   license "GPL-3.0-or-later"
 
   depends_on :macos  
