cdf4be95b3f3b7bba0b55d6b0572c28977b6a57f
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-07-28 15:38:25 +0100

    update formula to v0.0.8

--- Formula/cli-utils.rb
+++ Formula/cli-utils.rb
@@ -1,8 +1,8 @@
 class CliUtils < Formula
   desc "A collection of useful CLI commands."
   homepage "https://github.com/benmoose/homebrew-cli-utils"
-  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "aa8c6a2d228cd8c0eaf093a8ec4b13f1229fe0ffe0497125d2c3ae8bccad5e38"
+  url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "53472ae9064844d154f6183ec56a3fafa5f9d6d5e98d6cba691562eb869d9fb6"
   license "GPL-3.0-or-later"
 
   depends_on :macos
