2d5ad4ee7e79bb3cb4d61cd3469df71caeb64db3
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-07-28 14:51:59 +0100

    fix ,spinner array

--- functions/uuid.zsh
+++ functions/uuid.zsh
@@ -9,18 +9,3 @@ function uuid {
     [[ "$1" != "-x" ]] && print -n "${out}" | pbcopy
 	[[ "$1" != "-q" ]] && print "${out}"
 }
-
-function ,spinner {
-    declare -r pid_arg="${1}" msg_arg="${2}" style_arg="${3:-$(\tput setaf 6)}" \
-        CIVIS="$(\tput civis)" CNORM="$(\tput cnorm)" CR="$(\tput cr)" EL="$(\tput el)" NS="$(\tput sgr0)"
-    declare -ar frames=("⠄", "⠆", "⠇", "⠋", "⠙", "⠸", "⠰", "⠠", "⠰", "⠸", "⠙", "⠋", "⠇", "⠆")
-    declare -i i
-
-    printf "${CIVIS}"  # hide cursor
-    \trap 'printf "${CNORM}"' EXIT ERR
-    while \kill -0 "${pid_arg}" 2>/dev/null; do
-        printf "${CR}%s%s${NS}" "${style_arg}" "${frames[$((i % ${#frames[@]} + 1))]}" "${msg_arg}"
-        ((i++))
-        \sleep 0.045
-    done
-}
