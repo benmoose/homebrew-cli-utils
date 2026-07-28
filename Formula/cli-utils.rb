ea07a1b4baa9d70fb8f2485519bbc3ac8063e42b
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-07-28 14:22:16 +0100

    update cli-utils test and install

--- Formula/cli-utils.rb
+++ Formula/cli-utils.rb
@@ -1,5 +1,5 @@
 class CliUtils < Formula
-  desc "Custom utility commands."
+  desc "A collection of useful CLI commands."
   homepage "https://github.com/benmoose/homebrew-cli-utils"
   url "https://github.com/benmoose/homebrew-cli-utils/archive/refs/tags/v0.0.6.tar.gz"
   sha256 "a7486586e5874551be0b78d631c4d3525af0735f5563cf3a3aa068c670b27b07"
@@ -8,7 +8,7 @@ class CliUtils < Formula
   depends_on :macos
 
   def install
-    (packageshare/"functions").install Dir["functions/*.zsh"]
+    (pkgshare/"functions").install Dir["functions/*.zsh"]
   end
 
   def caveats
@@ -24,6 +24,16 @@ class CliUtils < Formula
   end
 
   test do
-    system "zsh", "-c", "ls #{pkgshare}/functions"
+    (testpath/".zshrc").write <<~EOS
+      for f in #{pkgshare}/functions/*.zsh; do
+        source "$f"
+      done
+    EOS
+
+    output = shell_output("zsh -c 'source #{testpath}/.zshrc && type com cos rbm uuid'")
+    assert_match "com is a shell function", output
+    assert_match "cos is a shell function", output
+    assert_match "rbm is a shell function", output
+    assert_match "uuid is a shell function", output
   end
 end
