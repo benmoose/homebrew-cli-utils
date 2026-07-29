fd5c56665321ee54f057981d741750e2e6c855e8
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-07-29 06:35:27 +0100

    fix incorrect return code when calling uuid -q

--- src/functions/uuid.zsh
+++ src/functions/uuid.zsh
@@ -5,10 +5,18 @@ set -o pipefail
 alias uid="uuid"
 
 function uuid {
-	if ! which uuidgen &>/dev/null; then return 1; fi
+	if ! which uuidgen &>/dev/null; then
+		,err "$0: uuidgen not found"
+		return 1
+	fi
 
-	declare -r out=$(uuidgen | tr "[:upper:]" "[:lower:]")
+	declare -r out=$(uuidgen | tr -d '\n')
 
-	[[ "$1" != "-x" ]] && print -n "${out}" | pbcopy
-	[[ "$1" != "-q" ]] && print "${out}"
+	if [[ "$1" != "-x" ]]; then
+		printf "${out:l}" | pbcopy
+	fi
+	
+	if [[ "$1" != "-q" ]]; then
+		printf "${out:l}\n"
+	fi
 }
