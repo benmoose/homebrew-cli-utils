4553813a13380bd97b308dc44ce4bf9db16a9c46
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-08-04 13:22:34 +0100

    move tput consts to init.sh

--- functions/init.sh
+++ functions/init.sh
@@ -5,6 +5,10 @@
 local -r fn_dir="${0:A:h}/functions"
 
 if [[ -z ${fpath[(r)${fn_dir}]} ]]; then
+	declare -rx RED=$(\tput setaf 1) GREEN=$(\tput setaf 2) YELLOW=$(\tput setaf 3) \
+		CYAN=$(\tput setaf 6) BOLD=$(\tput bold) DIM=$(\tput dim) NS=$(\tput sgr0) \
+		CR=$(\tput cr) EL=$(\tput el) CIVIS=$(\tput civis) CNORM=$(\tput cnorm)
+
 	fpath+=("${fn_dir}")
 
 	for fn_file in "${fn_dir}"/*; do
