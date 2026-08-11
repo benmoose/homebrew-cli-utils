ea0a6e68c685649a3ce2aa415be449dd465aff01
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-08-11 04:36:19 +0100

    update init.sh

--- src/init.sh
+++ src/init.sh
@@ -3,24 +3,22 @@
 #   source "/path/to/init.sh"
 
 emulate -L zsh
-set -o errexit
 
 local -r fn_dir="${0:A:h}/functions"
 
 if [[ -z ${fpath[(r)${fn_dir}]} ]]; then
-	declare -rx RED=$(\tput setaf 1) GREEN=$(\tput setaf 2) YELLOW=$(\tput setaf 3) BLUE=$(\tput setaf 4) \
-		MAGENTA=$(\tput setaf 5) CYAN=$(\tput setaf 6) DEFAULT=$(\tput setaf 9) \
-		BOLD=$(\tput bold) DIM=$(\tput dim) \
-		CR=$(\tput cr) EL=$(\tput el) CIVIS=$(\tput civis) CNORM=$(\tput cnorm) NS=$(\tput sgr0)
+	declare -rx RED=$(tput setaf 1) GREEN=$(tput setaf 2) YELLOW=$(tput setaf 3) BLUE=$(tput setaf 4) \
+		MAGENTA=$(tput setaf 5) CYAN=$(tput setaf 6) \
+		BOLD=$(tput bold) DIM=$(tput dim) \
+		CR=$(tput cr) EL=$(tput el) CIVIS=$(tput civis) CNORM=$(tput cnorm) NS=$(tput sgr0)
 
-	if ! [[ -d "${fn_dir}" ]]; then
+	if [[ -d "${fn_dir}" ]]; then
+		fpath+=("${fn_dir}")
+
+		for fn_file in "${fn_dir}"/*; do
+			emulate zsh -c "autoload -Uz -- ${fn_file:t}"
+		done
+	else
 		printf >&2 "%s%s: expected to find directory at %s, try reinstalling benmoose/cli-utils%s\n" "${RED}" "${0}" "${fn_dir}" "${NS}"
-		return 1
 	fi
-
-	fpath+=("${fn_dir}")
-
-	for fn_file in "${fn_dir}"/*; do
-		emulate zsh -c "autoload -Uz -- ${fn_file:t}"
-	done
 fi
