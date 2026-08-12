0de2721a75fc84c6b47f7c25e8caa34eb22a09e6
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-08-12 09:26:04 +0100

    bump formula to v0.0.43

--- src/fn-utils.sh
+++ src/fn-utils.sh
@@ -4,19 +4,19 @@
 
 emulate -L zsh
 
-local fn fn_arg="$1" \
-	fn_dir="$(brew --prefix)/share/zsh/site-functions"
+local fn fn_arg="$1"
 
-if [[ -z ${fpath[(r)${fn_dir}]} ]]; then
-	fpath=($fpath "${fn_dir}")
+if [[ -z ${fpath[(r)${fn_arg}]} ]]; then
+	fpath=($fpath "${fn_arg}")
 fi
 
 for fn in "${fn_arg}"/*; do
 	emulate zsh -c "autoload -Uz ${fn:t}"
 done
 
-local -r tputenv=$(env | egrep 'RED|GREEN|YELLOW|BLUE|CYAN|BOLD|DIM|CR|EL|NS' -wc)
-if [[ "${tputenv}" == "10" ]]; then
+local _tputenv=$(env | egrep 'RED|GREEN|YELLOW|BLUE|CYAN|BOLD|DIM|CR|EL|NS' -wc)
+
+if [[ "${_tputenv}" == "10" ]]; then
 	declare -rx RED=$(tput setaf 1) GREEN=$(tput setaf 2) YELLOW=$(tput setaf 3) BLUE=$(tput setaf 4) \
 		MAGENTA=$(tput setaf 5) CYAN=$(tput setaf 6) BOLD=$(tput bold) DIM=$(tput dim) \
 		CR=$(tput cr) EL=$(tput el) CIVIS=$(tput civis) CNORM=$(tput cnorm) NS=$(tput sgr0)
