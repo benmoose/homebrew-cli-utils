b9e92955859ed0782e07d9e2be7368b29775c73d
Ben Hadfield <16048190+benmoose@users.noreply.github.com>
2026-08-26 07:27:47 +0100

    add init-env.zsh

---
+++ src/init-env.zsh
@@ -0,0 +1,15 @@
+set -a
+CR=$(tput cr)
+EL=$(tput el)
+CIVIS=$(tput civis)
+CNORM=$(tput cnorm)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+BOLD=$(tput bold)
+DIM=$(tput dim)
+NS=$(tput sgr0)
+set +a
