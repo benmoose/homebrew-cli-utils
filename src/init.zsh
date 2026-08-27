4242811759cc08388b3c81787cd45d5dd4761ebd
Ben "Moose" Hadfield <16048190+benmoose@users.noreply.github.com>
2026-08-27 21:44:34 +0100

    shrink init.zsh

--- src/init.zsh
+++ src/init.zsh
@@ -1,54 +1,29 @@
 #!/usr/bin/env zsh
+emulate -L zsh
 
-0="${(%):-%N}"
-
-if [[ -z ${ZSH_VERSION-} ]]; then
-	command printf "${0:t}: expect zsh shell\n" >&2; return 1
+if [[ -z "$ZSH_VERSION" ]]; then
+	command printf "${0:t}: expect zsh shell\n" >&2
+	return 1
 fi
 
-_is_sourced() {
-	[[ "$zsh_eval_context" == *file* ]]
-}
-
 _init() {
-	emulate -L zsh
-	set -u
-	local -r \
-		prefix="${1:=$PREFIX}" \
-		func_dir="$PREFIX/share/zsh/site-functions" \
-		pkgshare="$PREFIX/share/cli-utils"
-
-	echo "> prefix=$prefix"
-	echo "> func_dir=$func_dir"
-	echo "> pkgshare=$pkgshare"
-
+	local -r func_dir="${1:h}/share/zsh/site-functions"
 	if ! [[ -d "$func_dir" ]]; then
 		command printf \
-			"fatal: installed functions not found, expect directory at %s. Try running \"brew reinstall cli-utils\"\n" \
-			"$func_dir" >&2
+			"%s: installed functions not found, expect directory at %s.\nTry running \"brew reinstall cli-utils\"\n" \
+			"${1:t}" "$func_dir" >&2
 		return 1
 	fi
 
 	export -TU FPATH fpath
-	if [[ -z ${fpath[(r)$func_dir]-} ]]; then
-		fpath+=( "${func_dir}" )
-	fi
-
-	# if [[ "$(printenv | egrep 'RED|GREEN|YELLOW|BLUE|CYAN|BOLD|DIM|CR|EL|CIVIS|CNORM|NS' -wc)" != "12" ]]; then
-	# 	set -a
-	# 	source "${pkgshare}/init-env.zsh"
-	# 	set +a
-	# fi
-
-	builtin autoload -Uz "${func_dir:a}"/*(:t)
+	[[ -n "${fpath[(r)$func_dir]-}" ]] || fpath+=( "${func_dir}" )
+	builtin autoload -Uz ${func_dir}/*
+	source "${1:h}/share/cli-utils/init-env.zsh"
 }
 
 {
-	_is_sourced || return 1
-
-	prefix="$(brew --prefix cli-utils)"
-	source "$prefix/share/cli-utils/init-env.zsh"
-	PREFIX="$prefix" _init
+	[[ "${zsh_eval_context[-1]}" == "file" } ]] || return 1
+	_init ${(%):-%N}
 } always {
-	unset -f _is_sourced _init
+	unset -f _init
 }
